733ba8084a274ff675059bbcc7d2339dd798df88
Johan Erlangga <johanerlangga12@gmail.com>
2024-09-10 20:38:27 +0700

    get list

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -22,25 +22,53 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Album>(
-      future: futureAlbum,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(snapshot.data!.title),
-              Image.network(snapshot.data!.url),
-            ],
-          ));
-        } else if (snapshot.hasError) {
-          return Center(child: Text('${snapshot.error}'));
-        }
+    return FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(itemBuilder: (context, index) {
+              return Container(
+                color: Colors.yellow,
+                margin: EdgeInsets.symmetric(vertical: 12),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Text(snapshot.data![index].title),
+                      Image.network(
+                        snapshot.data![index].url,
+                        height: 20,
+                        width: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            });
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        });
 
-        // By default, show a loading spinner.
-        return const CircularProgressIndicator();
-      },
-    );
+    // FutureBuilder<Album>(
+    //   future: futureAlbum,
+    //   builder: (context, snapshot) {
+    //     if (snapshot.hasData) {
+    //       return
+    //       Center(
+    //           child: Column(
+    //         mainAxisAlignment: MainAxisAlignment.center,
+    //         children: [
+    //           Text(snapshot.data!.title),
+    //           Image.network(snapshot.data!.url),
+    //         ],
+    //       ));
+    //     } else if (snapshot.hasError) {
+    //       return Center(child: Text('${snapshot.error}'));
+    //     }
+
+    //     // By default, show a loading spinner.
+    //     return const CircularProgressIndicator();
+    //   },
+    // );
   }
 }
--- lib/services/api.dart
+++ lib/services/api.dart
@@ -8,12 +8,20 @@ Future<Album> fetchAlbum() async {
       .get(Uri.parse('https://jsonplaceholder.typicode.com/photos/1'));
 
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
     return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
     throw Exception('Failed to load album');
+  }
+}
+
+Future<List<Album>> getData() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+
+  if (response.statusCode == 200) {
+    List jsonResponse = jsonDecode(response.body) as List;
+    return jsonResponse.map((data) => Album.fromJson(data)).toList();
+  } else {
+    return throw Exception('Failed to load album');
   }
 }
